9f810f4681db875f1c251e211aed7d0722afc470
dylan mitchley <dylanmitchley1994@gmail.com>
2025-06-30 23:35:40 +0200

    update

---
+++ main.py
@@ -0,0 +1,19 @@
+def get_book_text(f):
+     
+    return f.read()
+
+def count_words(text):
+    
+    words = text.split()
+    return len(words)
+
+def main():
+    path_to_file = "./books/frankenstein.txt"
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        contents = get_book_text(f)   
+    
+    num_words = count_words(contents)
+    print(f"{num_words} words found in the document")
+
+if __name__ == "__main__":
+    main()
