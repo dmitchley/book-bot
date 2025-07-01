5a6ba7a8845dd7093cf1866a4ddcd591dfd13dfc
dylan mitchley <dylanmitchley1994@gmail.com>
2025-07-02 01:34:51 +0200

    file cleanup

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from stats import get_book_text
 from stats import count_words
-from stats import count_chars
-from stats import new_function
+from stats import count_types
+ 
  
 def main():
     path_to_file = "./books/frankenstein.txt"
     with open(path_to_file, 'r', encoding='utf-8') as f:
         contents = get_book_text(f)   
-    
+      
     num_words = count_words(contents)
-    #print(f"{num_words} words found in the document")
+     
 
-    counted = count_chars(contents)
-    #print(f'numbers return is : {counted}')
+    item_return = count_types(contents)
+    print(item_return)
 
-    newsht = new_function(counted)
-    print(newsht)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -5,36 +5,20 @@ def get_book_text(f):
 def count_words(text):
     
     words = text.split()
-    #new = text.replace(" ", "")
-    #print(new)
     return len(words)
 
-word_counts = {}
-def count_chars(text):
-    #loop_text = text.split()
-    
-    words = text.replace(" ", "")
-    
-    for word in words:
-        word = word.lower().strip(".,!?")  
-        if word in word_counts:
-            word_counts[word] += 1
+def count_types(texts):
+    counts = {}
+
+    for item in texts:
+        if item in counts:
+            counts[item] += 1
         else:
-            word_counts[word] = 1
-
-    return str(word_counts)
-
-
-def new_function(inputstuff):
-
+            counts[item] = 1
+    
      
+    sorted_dict = dict(sorted(counts.items()))
+     
+    return sorted_dict
 
-    new = inputstuff.split()
-    print(new.sort())
-    #print(new[0::2])
-    print(new.values())
-    #print(new.sort(reverse=True, new.values()))
-    return inputstuff
  
-
-#new_function(word_counts)
